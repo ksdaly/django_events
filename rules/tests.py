9da32d80216ddecd21e871f36a35b1fd7fe13dd2
Kate Daly <kate.s.daly@gmail.com>
2018-09-28 15:38:38 -0400

    fix: tests

--- rules/tests.py
+++ rules/tests.py
@@ -4,30 +4,42 @@ from django.test import TestCase
 from django.utils import timezone
 from django.urls import reverse
 
-from .models import Rule
+from .models import Rule, Quantity
 
-def create_rule(data=None):
+def create_rule(props=None):
     """
     Create a rule
     """
     defaults = {
-        'qty': 1,
-        'name': 'Coffee',
-        'amount': 199,
-        'occurred_at': datetime.datetime.now().isoformat()
+        'handler_type': 'Quantity',
+        'data': {
+            'qty': 1,
+            'name': 'Coffee',
+            'amount': 199,
+            'occurred_at': datetime.datetime.now().isoformat()
+        }
     }
 
-    if data: defaults.update(data)
+    if props: defaults.update(props)
 
-    return Rule.objects.create(data=defaults)
+    return Rule.objects.create(**defaults)
 
 class RuleModelTests(TestCase):
 
     def test_was_created(self):
         """
-        create a rule
+        Create a rule
         """
         rule = create_rule()
         self.assertTrue(
             Rule.objects.filter(pk = rule.pk).exists()
         )
+
+    def test_has_handler(self):
+        """
+        Initializes and caches handler property for specified handler with rule data
+        """
+        rule = create_rule()
+        self.assertIsInstance(rule.handler, Quantity)
+        self.assertEqual(rule.handler.qty, rule.data['qty'])
+        self.assertIs(rule.handler, rule.handler)
