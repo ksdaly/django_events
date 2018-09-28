7d8778b223dc98223908b12bde85da8885d06ea0
Kate Daly <kate.s.daly@gmail.com>
2018-09-28 15:54:06 -0400

    fix: rule handler tests

--- rules/tests.py
+++ rules/tests.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 from django.utils import timezone
 from django.urls import reverse
 
-from .models import Rule, Quantity
+from .models import Rule, Quantity, Category, DateTime
 
 def create_rule(props=None):
     """
@@ -13,10 +13,7 @@ def create_rule(props=None):
     defaults = {
         'handler_type': 'Quantity',
         'data': {
-            'qty': 1,
-            'name': 'Coffee',
-            'amount': 199,
-            'occurred_at': datetime.datetime.now().isoformat()
+            'qty': 1
         }
     }
 
@@ -43,3 +40,93 @@ class RuleModelTests(TestCase):
         self.assertIsInstance(rule.handler, Quantity)
         self.assertEqual(rule.handler.qty, rule.data['qty'])
         self.assertIs(rule.handler, rule.handler)
+
+class QuantityTests(TestCase):
+
+    def test_is_eligible(self):
+        handler = Quantity(**{'qty': 1})
+        event = {
+            'qty': 1,
+            'name': 'Coffee',
+            'amount': 199,
+            'occurred_at': datetime.datetime.now().isoformat()
+        }
+
+        self.assertTrue(
+            handler.is_eligible(event)
+        )
+
+    def test_is_not_eligible(self):
+        handler = Quantity(**{'qty': 2})
+        event = {
+            'qty': 1,
+            'name': 'Coffee',
+            'amount': 199,
+            'occurred_at': datetime.datetime.now().isoformat()
+        }
+
+        self.assertFalse(
+            handler.is_eligible(event)
+        )
+
+class CategoryTests(TestCase):
+
+    def test_is_eligible(self):
+        handler = Category(**{'names': 'Coffee'})
+        event = {
+            'qty': 1,
+            'name': 'Coffee',
+            'amount': 199,
+            'occurred_at': datetime.datetime.now().isoformat()
+        }
+
+        self.assertTrue(
+            handler.is_eligible(event)
+        )
+
+    def test_is_not_eligible(self):
+        handler = Category(**{'names': 'Bagel'})
+        event = {
+            'qty': 1,
+            'name': 'Coffee',
+            'amount': 199,
+            'occurred_at': datetime.datetime.now().isoformat()
+        }
+
+        self.assertFalse(
+            handler.is_eligible(event)
+        )
+
+class DateTimeTests(TestCase):
+
+    def test_is_eligible(self):
+        now = datetime.datetime.now()
+        starts_at = now - datetime.timedelta(days=1)
+        ends_at = now + datetime.timedelta(days=1)
+        handler = DateTime(**{'starts_at': starts_at.isoformat(), 'ends_at': ends_at.isoformat()})
+        event = {
+            'qty': 1,
+            'name': 'Coffee',
+            'amount': 199,
+            'occurred_at': now.isoformat()
+        }
+
+        self.assertTrue(
+            handler.is_eligible(event)
+        )
+
+    def test_is_not_eligible(self):
+        now = datetime.datetime.now()
+        starts_at = now - datetime.timedelta(days=1)
+        ends_at = now + datetime.timedelta(days=1)
+        handler = DateTime(**{'starts_at': starts_at.isoformat(), 'ends_at': ends_at.isoformat()})
+        event = {
+            'qty': 1,
+            'name': 'Coffee',
+            'amount': 199,
+            'occurred_at': (ends_at + datetime.timedelta(days=1)).isoformat()
+        }
+
+        self.assertFalse(
+            handler.is_eligible(event)
+        )
